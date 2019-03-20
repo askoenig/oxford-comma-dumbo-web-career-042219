70370b505533efd01067cc978c515df1a0271c62
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-20 01:30:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,9 @@ def oxford_comma(array)
   array.join()
     elsif array.length == 2
     array.join(" and ")
-      elsif array.length == 3
-      new_string = ""
-      new_string << array.join(", ")
-      new_string.insert(14, "and ")
+      elsif array.length >= 3
+      a.insert(-1, `and`)
+      array.join(", ")
   end
 end
 
