22ce3a14071a046beb1da294a1a4230babdf13b3
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-20 01:41:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
       elsif array.length >= 3
       #array.insert(-2, "and")
-      array.(-2) << "and "
+      array.last << "and "
       array.join(", ")
   end
 end
